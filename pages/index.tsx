c670dcd4031ad13ae47256d7f25e328fd28471ff
Christopher David <chris@arcade.city>
2021-12-03 17:05:04 -0600

    Install Tailwind (#2)
    * Add tailwind
    
    * initial tailwind lander
    
    * initial copy
    
    * rotate map

--- pages/index.tsx
+++ pages/index.tsx
@@ -83,11 +83,65 @@ export default function Home(props) {
     // mapView.renderLabels = false
     window.onresize = () => mapView.resize(window.innerWidth, window.innerHeight)
 
+    // Rotate
+    setTimeout(() => {
+      mapView.addEventListener(harp.MapViewEventNames.AfterRender, () => {
+        mapView.lookAt({
+          heading: mapView.heading + 0.02,
+        })
+        mapView.update()
+      })
+    }, 1000)
+
     const vectorTileDataSource = new harp.VectorTileDataSource({
       authenticationCode: '_ZQeCfAB3nJFJ4E7JJ7W-CwSSW3vvUh6032RY85_OVs',
     })
     mapView.addDataSource(vectorTileDataSource)
   }, [harp])
+
+  return (
+    <div
+      className='flex h-screen w-screen justify-center items-center'
+      style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}
+    >
+      <main className='mx-auto max-w-7xl px-4'>
+        <div className='text-center'>
+          <h1 className='text-4xl tracking-tight font-extrabold text-gray-100 sm:text-5xl md:text-6xl'>
+            <span className='block xl:inline'>Connect Freely.</span>
+          </h1>
+          <p className='mt-3 max-w-md mx-auto text-base text-indigo-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl'>
+            Peer-to-Peer Rides and More
+          </p>
+          <div className='mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8'>
+            <div className='rounded-md shadow'>
+              <a
+                href='#'
+                className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'
+              >
+                Log in with Twitter
+              </a>
+            </div>
+            {/* <div className='rounded-md shadow'>
+              <a
+                href='#'
+                className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'
+              >
+                Get started
+              </a>
+            </div>
+            <div className='mt-3 rounded-md shadow sm:mt-0 sm:ml-3'>
+              <a
+                href='#'
+                className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10'
+              >
+                Live demo
+              </a>
+            </div> */}
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 
   return (
     <main>
