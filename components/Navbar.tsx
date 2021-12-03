b3240dcfe83511c25e4eb7baad5dfaeb0ee06d9c
Christopher David <chris@arcade.city>
2021-12-02 19:57:38 -0600

    Add firebase google auth w usernames

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
+import { useContext } from 'react'
+import { UserContext } from '../lib/context'
 
 // Top navbar
 export default function Navbar() {
-  const user = null
-  const username = null
+  const { user, username } = useContext(UserContext)
 
   return (
     <nav className='navbar'>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/globals.css'
 import Navbar from '../components/Navbar'
 import { Toaster } from 'react-hot-toast'
+import { UserContext } from '../lib/context'
+import { useUserData } from '../lib/hooks'
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData()
   return (
-    <>
+    <UserContext.Provider value={userData}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   )
 }
 
