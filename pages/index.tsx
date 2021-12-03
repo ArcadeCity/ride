072e3e692b73d82068372121f045f58397e41791
Christopher David <chris@arcade.city>
2021-12-03 16:57:52 -0600

    initial copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -97,15 +97,21 @@ export default function Home(props) {
       <main className='mx-auto max-w-7xl px-4'>
         <div className='text-center'>
           <h1 className='text-4xl tracking-tight font-extrabold text-gray-100 sm:text-5xl md:text-6xl'>
-            <span className='block xl:inline'>Data to enrich your</span>{' '}
-            <span className='block text-indigo-600 xl:inline'>online business</span>
+            <span className='block xl:inline'>Connect Freely.</span>
           </h1>
-          <p className='mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl'>
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
-            commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+          <p className='mt-3 max-w-md mx-auto text-base text-indigo-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl'>
+            Peer-to-Peer Rides and More
           </p>
           <div className='mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8'>
             <div className='rounded-md shadow'>
+              <a
+                href='#'
+                className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'
+              >
+                Log in with Twitter
+              </a>
+            </div>
+            {/* <div className='rounded-md shadow'>
               <a
                 href='#'
                 className='w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10'
@@ -120,7 +126,7 @@ export default function Home(props) {
               >
                 Live demo
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
       </main>
