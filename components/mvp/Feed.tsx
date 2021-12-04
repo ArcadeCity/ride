6f77a782cd2b8bae64d10cc9cb0446cbb8b147fd
Christopher David <chris@arcade.city>
2021-12-04 12:31:18 -0600

    extract Post from Feed

--- components/mvp/Feed.tsx
+++ components/mvp/Feed.tsx
@@ -1,5 +1,5 @@
 import PostBox from '@components/mvp/PostBox'
-import { ChatAltIcon } from '@heroicons/react/solid'
+import Post from './Post'
 
 export default function Feed() {
   return (
@@ -20,74 +20,7 @@ export default function Feed() {
                   />
                 ) : null}
                 <div className='relative flex items-start space-x-3'>
-                  {activityItem.type === 'post' ? (
-                    <>
-                      <div className='relative'>
-                        <img
-                          className='h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white'
-                          src={activityItem.imageUrl}
-                          alt=''
-                        />
-
-                        <span className='absolute -bottom-0.5 -right-1 bg-white rounded-tl px-0.5 py-px'>
-                          <ChatAltIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
-                        </span>
-                      </div>
-                      <div className='min-w-0 flex-1'>
-                        <div>
-                          <div className='text-sm'>
-                            <a
-                              target='_blank'
-                              href={activityItem.person.href}
-                              className='font-medium text-gray-900'
-                            >
-                              {activityItem.person.name}
-                            </a>
-                          </div>
-                          <p className='mt-0.5 text-sm text-gray-500'>
-                            Posted {activityItem.date} in {activityItem.city}
-                          </p>
-                        </div>
-                        <div className='mt-2 text-sm text-gray-700'>
-                          <p>{activityItem.comment}</p>
-                        </div>
-                      </div>
-                    </>
-                  ) : null}
-                  {activityItem.type === 'comment' ? (
-                    <>
-                      <div className='relative'>
-                        <img
-                          className='h-10 w-10 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white'
-                          src={activityItem.imageUrl}
-                          alt=''
-                        />
-
-                        <span className='absolute -bottom-0.5 -right-1 bg-white rounded-tl px-0.5 py-px'>
-                          <ChatAltIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
-                        </span>
-                      </div>
-                      <div className='min-w-0 flex-1'>
-                        <div>
-                          <div className='text-sm'>
-                            <a
-                              target='_blank'
-                              href={activityItem.person.href}
-                              className='font-medium text-gray-900'
-                            >
-                              {activityItem.person.name}
-                            </a>
-                          </div>
-                          <p className='mt-0.5 text-sm text-gray-500'>
-                            Commented {activityItem.date} in {activityItem.city}
-                          </p>
-                        </div>
-                        <div className='mt-2 text-sm text-gray-700'>
-                          <p>{activityItem.comment}</p>
-                        </div>
-                      </div>
-                    </>
-                  ) : null}
+                  {activityItem.type === 'post' ? <Post activityItem={activityItem} /> : null}
                 </div>
               </div>
             </li>
@@ -110,7 +43,7 @@ const activity = [
   },
   {
     id: 4,
-    type: 'comment',
+    type: 'post',
     person: { name: 'JasonMeyers', href: '#' },
     imageUrl:
       'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
