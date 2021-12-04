56e4e62f160bd59296f6517def6a84e1dd18e1a4
Christopher David <chris@arcade.city>
2021-12-04 13:25:34 -0600

    navbar simplify

--- components/mvp/Navbar.tsx
+++ components/mvp/Navbar.tsx
@@ -1,24 +1,19 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
+import { Fragment, useContext } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import { PlusSmIcon } from '@heroicons/react/solid'
+import { UserContext } from '@lib/context'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  // { name: 'Dashboard', href: '#', current: true },
+  // { name: 'Team', href: '#', current: false },
+  // { name: 'Projects', href: '#', current: false },
+  // { name: 'Calendar', href: '#', current: false },
 ]
 const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
+  // { name: 'Your Profile', href: '#' },
+  // { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
 ]
 
@@ -26,9 +21,11 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function Navbar() {
+  const { user, username } = useContext(UserContext)
+  console.log(user)
   return (
-    <Disclosure as='nav' className='bg-gray-800'>
+    <Disclosure as='nav' className='bg-transparent'>
       {({ open }) => (
         <>
           <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
@@ -76,30 +73,30 @@ export default function Example() {
                 </div>
               </div>
               <div className='flex items-center'>
-                <div className='flex-shrink-0'>
+                {/* <div className='flex-shrink-0'>
                   <button
                     type='button'
                     className='relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500'
                   >
                     <PlusSmIcon className='-ml-1 mr-2 h-5 w-5' aria-hidden='true' />
-                    <span>New Job</span>
+                    <span>New Post</span>
                   </button>
-                </div>
+                </div> */}
                 <div className='hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center'>
-                  <button
+                  {/* <button
                     type='button'
                     className='bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'
                   >
                     <span className='sr-only'>View notifications</span>
                     <BellIcon className='h-6 w-6' aria-hidden='true' />
-                  </button>
+                  </button> */}
 
                   {/* Profile dropdown */}
                   <Menu as='div' className='ml-3 relative'>
                     <div>
                       <Menu.Button className='bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white'>
                         <span className='sr-only'>Open user menu</span>
-                        <img className='h-8 w-8 rounded-full' src={user.imageUrl} alt='' />
+                        <img className='h-8 w-8 rounded-full' src={user?.photoURL ?? ''} alt='' />
                       </Menu.Button>
                     </div>
                     <Transition
@@ -157,11 +154,11 @@ export default function Example() {
             <div className='pt-4 pb-3 border-t border-gray-700'>
               <div className='flex items-center px-5 sm:px-6'>
                 <div className='flex-shrink-0'>
-                  <img className='h-10 w-10 rounded-full' src={user.imageUrl} alt='' />
+                  <img className='h-10 w-10 rounded-full' src={user?.photoURL ?? ''} alt='' />
                 </div>
                 <div className='ml-3'>
-                  <div className='text-base font-medium text-white'>{user.name}</div>
-                  <div className='text-sm font-medium text-gray-400'>{user.email}</div>
+                  <div className='text-base font-medium text-white'>{user?.name}</div>
+                  <div className='text-sm font-medium text-gray-400'>{user?.email}</div>
                 </div>
                 <button
                   type='button'
