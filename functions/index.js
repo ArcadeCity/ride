90b5335905998d9b7fb926c3a213dc50509e2755
Christopher David <chris@arcade.city>
2021-12-04 15:09:00 -0600

    clean up fb functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,3 @@
-// require('dotenv').config()
-
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const serviceAccount = require('./acride-key.json')
@@ -7,14 +5,6 @@ const { MAGICSECRET } = require('./secrets')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-})
-
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
 })
 
 exports.auth = functions.https.onCall(async (data, context) => {
