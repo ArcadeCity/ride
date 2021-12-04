eac24906c33dc90a8155b9aa67902ba3b243bcc5
Christopher David <chris@arcade.city>
2021-12-04 14:07:02 -0600

    Initial magic firebase check

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { magic } from '@lib/magic'
 import Feed from '@components/mvp/Feed'
 import LoginHero from '@components/mvp/LoginHero'
+import { auth, functions } from '@lib/firebase'
 
 export default function HomePage() {
   const [userMetadata, setUserMetadata] = useState()
@@ -13,10 +14,20 @@ export default function HomePage() {
 
   useEffect(() => {
     // On mount, we check if a user is logged in.
-    // If so, we'll retrieve the authenticated user's profile.
-    magic.user.isLoggedIn().then((magicIsLoggedIn) => {
+    // If so, we'll auth with Firebase the authenticated user's profile.
+    magic.user.isLoggedIn().then(async (magicIsLoggedIn) => {
       if (magicIsLoggedIn) {
         magic.user.getMetadata().then(setUserMetadata)
+        console.log('Authing with Firebase...')
+        const didToken = await magic.user.getIdToken()
+        console.log('didToken:', didToken)
+        const authFunc = functions.httpsCallable('auth')
+        /* DID token is passed into the auth callable function */
+        let result = (await authFunc({ didToken })).data
+        console.log('result:', result)
+        /* Firebase user access token is used to authenticate */
+        const wat = await auth.signInWithCustomToken(result.token)
+        console.log('wat:', wat)
         setAuthed(true)
       }
     })
