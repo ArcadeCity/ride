7d61dff9f642ff735cfecf5cc4a621285f52ea6d
Christopher David <chris@arcade.city>
2021-12-04 02:19:46 -0600

    magic is any

--- lib/magic.ts
+++ lib/magic.ts
@@ -1,7 +1,7 @@
 import { Magic } from 'magic-sdk'
 import { OAuthExtension } from '@magic-ext/oauth'
 
-export const magic =
+export const magic: any =
   typeof window !== 'undefined'
     ? new Magic('pk_live_4C679FE162F04FDC', {
         extensions: [new OAuthExtension()],
