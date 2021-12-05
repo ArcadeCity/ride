9242c2509dc300bf367cb4a9be8bbca9700c3a7a
Christopher David <chris@arcade.city>
2021-12-05 17:12:53 -0600

    axe extra chat icon

--- components/mvp/Post.tsx
+++ components/mvp/Post.tsx
@@ -1,8 +1,6 @@
-import { ChatAltIcon } from '@heroicons/react/solid'
 import Image from 'next/image'
 
 export default function Post({ post }) {
-  console.log(post)
   if (!post.twitterMetadata) return <></>
   return (
     <>
@@ -17,10 +15,6 @@ export default function Post({ post }) {
             />
           </div>
         )}
-
-        <span className='absolute -bottom-0.5 -right-1 bg-white rounded-tl px-0.5 py-px'>
-          <ChatAltIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
-        </span>
       </div>
       <div className='min-w-0 flex-1'>
         <div>
