4f03e8624473d1a10960f64f27c97e809f103344
Christopher David <chris@arcade.city>
2021-12-05 11:34:36 -0600

    PostBox submits to firebase w dummy geoloc

--- components/mvp/PostBox.tsx
+++ components/mvp/PostBox.tsx
@@ -1,9 +1,40 @@
+import { auth, firestore, serverTimestamp } from '@lib/firebase'
 import { useStore } from '@lib/store'
+import { useForm } from 'react-hook-form'
+import toast from 'react-hot-toast'
 
 export default function PostBox() {
   const twitterMetadata = useStore((s) => s.oauthdata)
+  const geolocation = useStore((s) => s.geolocation)
+  const { register, errors, handleSubmit, formState, reset, watch } = useForm({
+    // defaultValues,
+    mode: 'onChange',
+  })
+
+  const { isValid, isDirty } = formState
+
+  const postsCollection = firestore
+    .collection('users')
+    .doc(auth.currentUser.uid)
+    .collection('posts')
+
+  const submitPost = async ({ content }) => {
+    const docRef = await postsCollection.add({
+      content,
+      geolocation,
+      updatedAt: serverTimestamp(),
+    })
+
+    console.log('auth.currentUser.uid:', auth.currentUser.uid)
+    console.log('postId:', docRef.id)
+
+    reset({ content })
+
+    toast.success('Post submitted successfully!')
+  }
+
   return (
-    <div className='flex items-start space-x-4 m-8'>
+    <div className='flex items-start space-x-4 m-8 max-w-xl'>
       <div className='flex-shrink-0'>
         <img
           className='inline-block h-10 w-10 rounded-full'
@@ -12,15 +43,20 @@ export default function PostBox() {
         />
       </div>
       <div className='min-w-0 flex-1'>
-        <form action='#' className='relative'>
+        <form onSubmit={handleSubmit(submitPost)} className='relative'>
           <div className='border border-gray-300 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500'>
             <label htmlFor='comment' className='sr-only'>
               Write a post
             </label>
             <textarea
+              name='content'
+              ref={register({
+                maxLength: { value: 20000, message: 'Post content is too long' },
+                minLength: { value: 10, message: 'Post content is too short' },
+                required: { value: true, message: 'Post content is required' },
+              })}
               rows={3}
-              name='comment'
-              id='comment'
+              id='content'
               className='block w-full py-3 border-0 resize-none focus:ring-0 sm:text-sm'
               placeholder='Write a post...'
               defaultValue={''}
@@ -37,6 +73,7 @@ export default function PostBox() {
 
           <div className='absolute bottom-0 inset-x-0 pl-3 pr-2 py-2 flex justify-between'>
             <div className='flex items-center space-x-5'>
+              {errors.content && <p className='text-red-500'>{errors.content.message}</p>}
               {/* <div className='flex items-center'>
                 <button
                   type='button'
@@ -51,6 +88,7 @@ export default function PostBox() {
               <button
                 type='submit'
                 className='inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                disabled={!isDirty || !isValid}
               >
                 Post
               </button>
