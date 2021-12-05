a522c24111df41863e9df5b8e24decee904c0805
Christopher David <chris@arcade.city>
2021-12-05 16:52:59 -0600

    add id to post

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -56,6 +56,7 @@ export function postToJSON(doc) {
   const data = doc.data()
   return {
     ...data,
+    id: doc.id,
     // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
     // createdAt: data?.createdAt.toMillis() || 0,
     updatedAt: data?.updatedAt.toMillis() || 0,
